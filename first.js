f29caf449aa93674fb7798b499403182b788d568
SAKSHISHARMA14 <dezzlingpalak@gmail.com>
2021-12-18 13:26:54 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const first = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h1>NAme</h1>
+        </div>
+    )
+}
+
+export default first
